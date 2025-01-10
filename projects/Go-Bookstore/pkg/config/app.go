ba5efb16d1ef9100a688ae20db698536c23c8b24
Devisri <devisris.it2022@citchennai.net>
2025-01-10 16:26:24 +0530

    Create app.go

---
+++ projects/Go-Bookstore/pkg/config/app.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "root:simplepassword@tcp(127.0.0.1:3306)/Bookstore?charset=utf8&parseTime=True&loc=UTC")
+
+	if err != nil {
+		panic(err)
+	}
+	db = d
+
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
